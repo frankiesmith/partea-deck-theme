9c98bb1b630759b32317d20d88dc61b38aa4b597
Frankie Smith <fsmith1989@gmail.com>
2018-06-21 02:32:50 -0400

    Added hero

--- theme/page.php
+++ theme/page.php
@@ -11,6 +11,8 @@ get_header(); ?>
 
   <div id="primary" class="content-area">
     <main id="main" class="site-main">
+    <div class="hero">
+    </div>
 
     <?php
     while ( have_posts() ) : the_post();
