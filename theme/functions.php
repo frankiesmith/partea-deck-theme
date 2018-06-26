7782412a40cd6b9b0610f6038befb6ce4d1fd4b5
Frankie Smith <fsmith1989@gmail.com>
2018-06-26 01:33:20 -0400

    Added CMB2 example functions

--- theme/functions.php
+++ theme/functions.php
@@ -1,6 +1,16 @@
 <?php
 
+/**
+ * Get the bootstrap!
+ * (Update path to use cmb2 or CMB2, depending on the name of the folder.
+ * Case-sensitive is important on some systems.)
+ */
+require_once __DIR__ . '/cmb2/init.php';
+
+
+
 add_theme_support( 'title-tag' );
+
 
 function fivehd_enqueue_scripts_styles() {
   // wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=' );
@@ -33,3 +43,63 @@ function fivehd_widgets_init() {
     ) );
 }
 add_action( 'widgets_init', 'fivehd_widgets_init' );
+
+
+add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
+/**
+ * Define the metabox and field configurations.
+ */
+function cmb2_sample_metaboxes() {
+
+  // Start with an underscore to hide fields from custom fields list
+  $prefix = '_yourprefix_';
+
+  /**
+   * Initiate the metabox
+   */
+  $cmb = new_cmb2_box( array(
+    'id'            => 'test_metabox',
+    'title'         => __( 'Test Metabox', 'cmb2' ),
+    'object_types'  => array( 'page', ), // Post type
+    'context'       => 'normal',
+    'priority'      => 'high',
+    'show_names'    => true, // Show field names on the left
+    // 'cmb_styles' => false, // false to disable the CMB stylesheet
+    // 'closed'     => true, // Keep the metabox closed by default
+  ) );
+
+  // Regular text field
+  $cmb->add_field( array(
+    'name'       => __( 'Test Text', 'cmb2' ),
+    'desc'       => __( 'field description (optional)', 'cmb2' ),
+    'id'         => $prefix . 'text',
+    'type'       => 'text',
+    'show_on_cb' => 'cmb2_hide_if_no_cats', // function should return a bool value
+    // 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
+    // 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
+    // 'on_front'        => false, // Optionally designate a field to wp-admin only
+    // 'repeatable'      => true,
+  ) );
+
+  // URL text field
+  $cmb->add_field( array(
+    'name' => __( 'Website URL', 'cmb2' ),
+    'desc' => __( 'field description (optional)', 'cmb2' ),
+    'id'   => $prefix . 'url',
+    'type' => 'text_url',
+    // 'protocols' => array('http', 'https', 'ftp', 'ftps', 'mailto', 'news', 'irc', 'gopher', 'nntp', 'feed', 'telnet'), // Array of allowed protocols
+    // 'repeatable' => true,
+  ) );
+
+  // Email text field
+  $cmb->add_field( array(
+    'name' => __( 'Test Text Email', 'cmb2' ),
+    'desc' => __( 'field description (optional)', 'cmb2' ),
+    'id'   => $prefix . 'email',
+    'type' => 'text_email',
+    // 'repeatable' => true,
+  ) );
+
+  // Add other metaboxes as needed
+
+}
