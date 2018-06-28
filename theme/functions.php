c9111f392e0d4d4f9a39de97779568deadc3f025
Frankie Smith <fsmith1989@gmail.com>
2018-06-28 16:29:03 -0400

    updated function names

--- theme/functions.php
+++ theme/functions.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/cmb2/init.php';
 add_theme_support( 'title-tag' );
 
 
-function fivehd_enqueue_scripts_styles() {
+function partea_enqueue_scripts_styles() {
   // wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=' );
   // wp_enqueue_script( 'font-awesome', 'https://use.fontawesome.com/releases/v5.0.6/js/all.js', array(), false, true );
 
@@ -23,16 +23,16 @@ function fivehd_enqueue_scripts_styles() {
 
     wp_enqueue_style( 'fivehdstarter', get_stylesheet_directory_uri() . '/theme.css' );
 }
-add_action( 'wp_enqueue_scripts', 'fivehd_enqueue_scripts_styles' );
+add_action( 'wp_enqueue_scripts', 'partea_enqueue_scripts_styles' );
 
-function fivehd_register_nav_menus() {
+function partea_register_nav_menus() {
     register_nav_menus( array(
         'main-navigation' => __( 'Primary Menu', 'fivehdstarter' ),
     ) );
 }
-add_action( 'after_setup_theme', 'fivehd_register_nav_menus' );
+add_action( 'after_setup_theme', 'partea_register_nav_menus' );
 
-function fivehd_widgets_init() {
+function partea_widgets_init() {
     register_sidebar( array(
         'name'          => __( 'Main Sidebar', 'textdomain' ),
         'id'            => 'main-sidebar',
@@ -42,7 +42,7 @@ function fivehd_widgets_init() {
         'after_title'   => '</h2>',
     ) );
 }
-add_action( 'widgets_init', 'fivehd_widgets_init' );
+add_action( 'widgets_init', 'partea_widgets_init' );
 
 
 add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
