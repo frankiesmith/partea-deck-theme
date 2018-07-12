ce209e55484e3d36f69d6fe3875b7b16da5feac3
Frankie Smith <fsmith1989@gmail.com>
2018-07-12 00:55:17 -0400

    beautified recipe html

--- theme/template-recipes.php
+++ theme/template-recipes.php
@@ -7,7 +7,7 @@ get_header(); ?>
 
   <div id="primary" class="content-area">
     <main id="main" class="site-main">
-<!--     <div class="hero">
+      <!--     <div class="hero">
       <?php
         $background_img = get_post_meta( get_the_ID(), '_partea_partea_header_image_id', true );
           printf(
@@ -18,9 +18,9 @@ get_header(); ?>
           echo "\n";
       ?>
     </div> -->
-    <div class="container">
+      <div class="container">
 
-    <?php
+        <?php
     while ( have_posts() ) : the_post();
 
       get_template_part( 'template-parts/content', 'page' );
@@ -30,245 +30,247 @@ get_header(); ?>
 
       </div>
 
-<div class="recipe-menu">
-  <a href="#jalepeno"><span class="jalepeno"></span></a>
-  <a href="#orange"><span class="orange"></span></a>
-  <a href="#ginger"><span class="ginger"></span></a>
-  <a href="#espresso"><span class="espresso"></span></a>
-  <a href="#wildberry"><span class="wildberry"></span></a>
+      <div class="recipe-menu">
+        <a href="#jalepeno"><span class="jalepeno"></span></a>
+        <a href="#orange"><span class="orange"></span></a>
+        <a href="#ginger"><span class="ginger"></span></a>
+        <a href="#espresso"><span class="espresso"></span></a>
+        <a href="#wildberry"><span class="wildberry"></span></a>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script>
-    function toggleRecipeMenu() {
-      if ($(this).scrollTop() > 300) {
-        $(".recipe-menu").fadeIn();
-      } else {
-        $(".recipe-menu").fadeOut();
-      }
-    }
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script>
+          function toggleRecipeMenu() {
+            if ($(this).scrollTop() > 300) {
+              $(".recipe-menu").fadeIn();
+            } else {
+              $(".recipe-menu").fadeOut();
+            }
+          }
 
-    $(window).scroll( toggleRecipeMenu );
+          $(window).scroll(toggleRecipeMenu);
 
-    $(toggleRecipeMenu);
-  </script>
+          $(toggleRecipeMenu);
+        </script>
 
-</div>
-
-
-
-<!--   Jalepeno Cards -->
-
-    <div id="jalepeno" class="jalepeno swiper-container">
-  <div class="swiper-wrapper">
-    <div class="swiper-slide">
-      <div class="recipe-image">
       </div>
-      <ul class="swiper-content">
-        <li class="recipe-name">Spicy Margarita</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <li>0.75oz fresh lime juice</li>
-        <li>0.75oz triple sec</li>
-        <div class="recipe-icon">
+
+
+
+      <!--   Jalepeno Cards -->
+
+      <div id="jalepeno" class="jalepeno swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="recipe-image">
+            </div>
+            <ul class="swiper-content">
+              <li class="recipe-name">Spicy Margarita</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <li>0.75oz fresh lime juice</li>
+              <li>0.75oz triple sec</li>
+              <div class="recipe-icon">
+              </div>
+            </ul>
+            <div class="recipe-footer">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="recipe-image">
+            </div>
+            <ul class="swiper-content">
+              <li class="recipe-name">Spicy & Pink</li>
+              <li>1.5oz Jalapeno vodka</li>
+              <li>0.5oz elderflower liqueur</li>
+              <li>3oz pink grapefruit juice</li>
+              <div class="recipe-icon">
+              </div>
+            </ul>
+            <div class="recipe-footer">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="recipe-image">
+            </div>
+            <ul class="swiper-content">
+              <li class="recipe-name">Spicy Bloody Mary</li>
+              <li>1.5oz Jalapeno vodka</li>
+              <li>4oz. tomato juice</li>
+              <li>Worcestershire sauce, horseradish, lemon and lime juice, celery salt, garlic powder</li>
+              <div class="recipe-icon">
+              </div>
+            </ul>
+            <div class="recipe-footer">
+            </div>
+          </div>
         </div>
-      </ul>
-      <div class="recipe-footer">
       </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="recipe-image">
-      </div>
-      <ul class="swiper-content">
-        <li class="recipe-name">Spicy & Pink</li>
-        <li>1.5oz Jalapeno vodka</li>
-        <li>0.5oz elderflower liqueur</li>
-        <li>3oz pink grapefruit juice</li>
-        <div class="recipe-icon">
+
+      <!--   Orange spice Cards -->
+
+      <div id="orange" class="orange swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="recipe-image">
+            </div>
+            <ul class="swiper-content">
+              <li class="recipe-name">Spicy Margarita</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <li>0.75oz fresh lime juice</li>
+              <li>0.75oz triple sec</li>
+              <div class="recipe-icon">
+              </div>
+            </ul>
+            <div class="recipe-footer">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="recipe-image">
+            </div>
+            <ul class="swiper-content">
+              <li class="recipe-name">Spicy & Pink</li>
+              <li>1.5oz Jalapeno vodka</li>
+              <li>0.5oz elderflower liqueur</li>
+              <li>3oz pink grapefruit juice</li>
+              <div class="recipe-icon">
+              </div>
+            </ul>
+            <div class="recipe-footer">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="recipe-image">
+            </div>
+            <ul class="swiper-content">
+              <li class="recipe-name">Spicy Bloody Mary</li>
+              <li>1.5oz Jalapeno vodka</li>
+              <li>4oz. tomato juice</li>
+              <li>Worcestershire sauce, horseradish, lemon and lime juice, celery salt, garlic powder</li>
+              <div class="recipe-icon">
+              </div>
+            </ul>
+            <div class="recipe-footer">
+            </div>
+          </div>
         </div>
-      </ul>
-      <div class="recipe-footer">
       </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="recipe-image">
-      </div>
-      <ul class="swiper-content">
-        <li class="recipe-name">Spicy Bloody Mary</li>
-        <li>1.5oz Jalapeno vodka</li>
-        <li>4oz. tomato juice</li>
-        <li>Worcestershire sauce, horseradish, lemon and lime juice, celery salt, garlic powder</li>
-        <div class="recipe-icon">
+
+      <!--   Ginger Cards -->
+
+      <div id="ginger" class="ginger swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="recipe-image">
+            </div>
+            <ul class="swiper-content">
+              <li class="recipe-name">Spicy Margarita</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <div class="recipe-icon">
+              </div>
+            </ul>
+            <div class="recipe-footer">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-content">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-content">
+            </div>
+          </div>
         </div>
-      </ul>
-      <div class="recipe-footer">
       </div>
-  </div>
-    </div>
-    </div>
 
-<!--   Orange spice Cards -->
+      <!--   Espresso Cards -->
 
-    <div id="orange" class="orange swiper-container">
-  <div class="swiper-wrapper">
-    <div class="swiper-slide">
-      <div class="recipe-image">
-      </div>
-      <ul class="swiper-content">
-        <li class="recipe-name">Spicy Margarita</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <li>0.75oz fresh lime juice</li>
-        <li>0.75oz triple sec</li>
-        <div class="recipe-icon">
+      <div id="espresso" class="espresso swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="recipe-image">
+            </div>
+            <ul class="swiper-content">
+              <li class="recipe-name">Spicy Margarita</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <div class="recipe-icon">
+              </div>
+            </ul>
+            <div class="recipe-footer">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-content">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-content">
+            </div>
+          </div>
         </div>
-      </ul>
-      <div class="recipe-footer">
       </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="recipe-image">
-      </div>
-      <ul class="swiper-content">
-        <li class="recipe-name">Spicy & Pink</li>
-        <li>1.5oz Jalapeno vodka</li>
-        <li>0.5oz elderflower liqueur</li>
-        <li>3oz pink grapefruit juice</li>
-        <div class="recipe-icon">
+
+      <!--   Wildberry Cards -->
+
+      <div id="wildberry" class="wildberry swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="recipe-image">
+            </div>
+            <ul class="swiper-content">
+              <li class="recipe-name">Spicy Margarita</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <li>1.5oz Jalapeno tequila</li>
+              <div class="recipe-icon">
+              </div>
+            </ul>
+            <div class="recipe-footer">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-content">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-content">
+            </div>
+          </div>
         </div>
-      </ul>
-      <div class="recipe-footer">
       </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="recipe-image">
-      </div>
-      <ul class="swiper-content">
-        <li class="recipe-name">Spicy Bloody Mary</li>
-        <li>1.5oz Jalapeno vodka</li>
-        <li>4oz. tomato juice</li>
-        <li>Worcestershire sauce, horseradish, lemon and lime juice, celery salt, garlic powder</li>
-        <div class="recipe-icon">
-        </div>
-      </ul>
-      <div class="recipe-footer">
-      </div>
-  </div>
-    </div>
-    </div>
 
-<!--   Ginger Cards -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.js"></script>
+      <script>
+        $(document).ready(function() {
+          var swiper = new Swiper('.swiper-container', {
+            effect: 'coverflow',
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            },
+            pagination: {
+              el: '.swiper-pagination',
+            },
+          });
 
-    <div id="ginger" class="ginger swiper-container">
-  <div class="swiper-wrapper">
-    <div class="swiper-slide">
-      <div class="recipe-image">
-      </div>
-      <ul class="swiper-content">
-        <li class="recipe-name">Spicy Margarita</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <div class="recipe-icon">
-        </div>
-      </ul>
-      <div class="recipe-footer">
-      </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="swiper-content">
-      </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="swiper-content">
-      </div>
-  </div>
-    </div>
-    </div>
-
-<!--   Espresso Cards -->
-
-    <div id="espresso" class="espresso swiper-container">
-  <div class="swiper-wrapper">
-    <div class="swiper-slide">
-      <div class="recipe-image">
-      </div>
-      <ul class="swiper-content">
-        <li class="recipe-name">Spicy Margarita</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <div class="recipe-icon">
-        </div>
-      </ul>
-      <div class="recipe-footer">
-      </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="swiper-content">
-      </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="swiper-content">
-      </div>
-  </div>
-    </div>
-    </div>
-
-<!--   Wildberry Cards -->
-
-    <div id="wildberry" class="wildberry swiper-container">
-  <div class="swiper-wrapper">
-    <div class="swiper-slide">
-      <div class="recipe-image">
-      </div>
-      <ul class="swiper-content">
-        <li class="recipe-name">Spicy Margarita</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <li>1.5oz Jalapeno tequila</li>
-        <div class="recipe-icon">
-        </div>
-      </ul>
-      <div class="recipe-footer">
-      </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="swiper-content">
-      </div>
-  </div>
-        <div class="swiper-slide">
-      <div class="swiper-content">
-      </div>
-  </div>
-    </div>
-    </div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.js"></script>
-<script>
-$(document).ready(function() {
- var swiper = new Swiper('.swiper-container', {
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows : true,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    });
-
-});
-</script>
+        });
+      </script>
 
 
-    </main><!-- #main -->
-  </div><!-- #primary -->
+    </main>
+    <!-- #main -->
+  </div>
+  <!-- #primary -->
 
-<?php
+  <?php
 get_sidebar();
 get_footer();
