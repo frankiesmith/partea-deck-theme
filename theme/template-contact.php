ba7a24f1847e0aee3516535f8d23d55e663ff2c0
Frankie Smith <fsmith1989@gmail.com>
2018-07-15 21:57:52 -0400

    Set up contact page

---
+++ theme/template-contact.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: Contact
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main">
+    <div class="hero">
+      <?php
+        $background_img = get_post_meta( get_the_ID(), '_partea_partea_header_image_id', true );
+          printf(
+            '<div class="hero-image" style="background-image:url(%s);"></div>',
+            wp_get_attachment_image_url( $background_img, 'large' )
+          );
+
+          echo "\n";
+      ?>
+    </div>
+    <div class="contact container">
+    <div class="column">
+          <?php
+    while ( have_posts() ) : the_post();
+
+      get_template_part( 'template-parts/content', 'page' );
+
+    endwhile; // End of the loop.
+    ?>
+    </div>
+    <div class="column">
+      <div class="contact-image">
+      </div>
+    </div>
+
+  </div>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
