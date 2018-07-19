35f662eaf94b17489261425efef2a5b025ae79ba
Frankie Smith <fsmith1989@gmail.com>
2018-07-19 00:13:54 -0400

    Footer styles/ added social icons

--- theme/footer.php
+++ theme/footer.php
@@ -11,19 +11,39 @@
 
 ?>
 
-  </div><!-- #content -->
+	</div>
+	<!-- #content -->
 
-  <footer id="colophon" class="footer">
-    <div class="copyright">
-    <p>© 2018, ParTea Booze Infusers.</p>
-  </div>
+	<footer id="colophon" class="footer">
+		<div class="container">
+		<div class="copyright">
+			<p>© 2018, ParTea Booze Infusers.</p>
+		</div>
+		<div class="social-icons">
+			<a href=" " target="_blank">
+				<?php echo file_get_contents( get_template_directory() . '/img/instagram.svg' ); ?>
+			</a>
+			<a href=" " target="_blank">
+				<?php echo file_get_contents( get_template_directory() . '/img/facebook.svg' ); ?>
+			</a>
+			<a href=" " target="_blank">
+				<?php echo file_get_contents( get_template_directory() . '/img/pinterest.svg' ); ?>
+			</a>
+			<a href=" " target="_blank">
+				<?php echo file_get_contents( get_template_directory() . '/img/twitter.svg' ); ?>
+			</a>
+		</div>
 
-    </div><!-- .site-info -->
-  </footer><!-- #colophon -->
-</div><!-- #page -->
+		</div>
+		<!-- .site-info -->
+	</footer>
+	<!-- #colophon -->
+	</div>
+	<!-- #page -->
 
-<?php wp_footer(); ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src="js/scripts.js"></script>
-</body>
-</html>
+	<?php wp_footer(); ?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script src="js/scripts.js"></script>
+	</body>
+
+	</html>
