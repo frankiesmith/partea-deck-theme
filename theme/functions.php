43ce3dc2bc4c183168dbdeed5f48b2151d30853e
Frankie Smith <fsmith1989@gmail.com>
2018-09-18 18:43:24 -0400

    cmb2 path

--- theme/functions.php
+++ theme/functions.php
@@ -5,7 +5,7 @@
  * (Update path to use cmb2 or CMB2, depending on the name of the folder.
  * Case-sensitive is important on some systems.)
  */
-require_once __DIR__ . '/cmb2/init.php';
+require_once __DIR__ . 'theme/cmb2/init.php';
 
 
 
